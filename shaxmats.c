cfbcad86257574b868ab1f7c80021806d409ac19
Сырых Марина Викторовна <ip712s24@mwp05.csc.local>
2018-02-26 09:25:58 +0700

    one commit

---
+++ shaxmats.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+  printf ("  A B C D E F G H\n ");
+  printf ("1 R N B Q K B N R\n ");
+  printf ("2 P P P P P P P P\n ");
+  printf ("3\n4\n5\n6\n ");
+  printf ("7 p p p p p p p p\n ");
+  printf ("8 r n b k q b n r ");
+  return 0;
+}
